30a99eb44bc898693725a6f751a526f88dd18287
Takahito Nakano <kano@ponko2.jp>
2016-02-17 23:52:05 +0900

    サイコロコマンドを追加

---
+++ commands/roll.js
@@ -0,0 +1,17 @@
+import Dice from 'node-dice-js';
+
+module.exports = controller => {
+  controller.on('slash_command', (bot, message) => {
+    if (message.command !== '/roll') {
+      return;
+    }
+
+    const dice = new Dice();
+
+    const result = dice.execute(message.text).text
+      .replace(/The result of /, '')
+      .replace(/ is /, ' = ');
+
+    bot.replyPublic(message, `<@${message.user}> ${result}`);
+  });
+};
